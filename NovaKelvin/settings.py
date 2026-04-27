626e50994df691a91e183131a468768c5b91314f
Yuki Suter <yuki@yukisuter.com>
2026-04-27 22:21:42 +0100

    added wip block

--- NovaKelvin/settings.py
+++ NovaKelvin/settings.py
@@ -59,6 +59,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'NovaKelvin.middleware.UnderConstructionMiddleware',
 ]
 
 ROOT_URLCONF = 'NovaKelvin.urls'
